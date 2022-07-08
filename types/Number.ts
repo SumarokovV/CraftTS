3a08d673974373c2b698a058fa08d45928c15713
SumarokovV <sumarokov.world@gmail.com>
2022-07-08 08:29:15 +0300

    type Uint

--- types/Number.ts
+++ types/Number.ts
@@ -1,4 +1,11 @@
+import { Split } from './String';
+
 export type NumberRange<
   TO extends number,
   R extends number[] = [],
 > = TO extends R['length'] ? [...R, TO][number] : NumberRange<TO, [...R, R['length']]>;
+
+export type Uint<
+  T extends number,
+  I extends string[] = Split<`${T}`>,
+> = I[0] extends '-' ? never : T;
